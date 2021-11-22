fa27bbacb6532f496f24c177a84594b376820fcf
dslamont <don.lamont@e-pict.net>
2021-11-22 12:05:08 +0000

    Add project files.

---
+++ SingleLineIfExample/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("");
+Console.WriteLine("Single Line 'if' Statement");
+Console.WriteLine("");
+
+string condition1 = "Some random text";
+Console.WriteLine($"condition1 = {condition1}");
+
+if (condition1 != null)
+{
+    Console.WriteLine($"This line should only appear if 'condition1; is not null.");
+}
+
+Console.WriteLine("");
+Console.WriteLine("");
